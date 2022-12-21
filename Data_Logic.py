99d17461fb96ecbc00749a27b1ecd33d1245caf5
Trololo23 <egorbeketov37@gmail.com>
2022-12-21 18:03:23 +0300

    Basic functionality

--- Data_Logic.py
+++ Data_Logic.py
@@ -37,24 +37,43 @@ class UsersData:
 
     def AddChatGame(self, chat_id):
       self.chat_players_map[chat_id] = list()
-      self.players_turn[chat_id] = 0
+      self.players_turn[chat_id] = list()
 
     def AddUser(self, chat_id, user_id):
-      for key in self.chat_players_map.keys():
-        if key == chat_id:
-          self.chat_players_map[key].append(user_id)
+      self.chat_players_map[chat_id].append(user_id)
 
-    def NextStep(self, chat_id, user_id):
-      cur_player = self.players_turn[chat_id]
-      players_numb = len(self.chat_players_map)
-      if self.chat_players_map[cur_player] != user_id:
+    def Check(self, chat_id):
+      if len(self.chat_players_map[chat_id]) == self.players_turn[chat_id][0]:
         return False
-      self.players_turn[chat_id] = (cur_player + 1) % players_numb
+      else:
+        return True
+
+    def AddPlayersNumb(self, chat_id, players_numb):
+      self.players_turn[chat_id] = list()
+      self.players_turn[chat_id].append(players_numb)
+      self.players_turn[chat_id].append(0)
+
+    def CheckTurn(self, chat_id, user_id):
+      cur_player = self.players_turn[chat_id][1] 
+      players_numb = self.players_turn[chat_id][0] 
+      if self.chat_players_map[chat_id][cur_player] != user_id:
+        return False
       return True
+
+    def NextStep(self, chat_id):
+      cur_player = self.players_turn[chat_id][1] 
+      players_numb = self.players_turn[chat_id][0]
+      self.players_turn[chat_id][1] = (cur_player + 1) % players_numb
 
     def CLear(self, chat_id):
       self.chat_players_map[chat_id] = list()
       self.players_turn[chat_id] = list()
+
+    def CheckState(self, chat_id):
+      if chat_id in self.players_turn.keys():
+        if len(self.players_turn[chat_id]) > 0:
+          return True
+      return False
 
 
 #Operates with towns used during game
@@ -73,6 +92,9 @@ class UsedTownsData:
       else:
         return False
 
+    def Start(self, chat_id):
+      self.used_towns[chat_id] = list()
+
     def Add(self, chat_id, town_name):
       self.used_towns[chat_id].append(town_name)
 
@@ -82,6 +104,10 @@ class UsedTownsData:
         return False
       return True
 
+    def GetLetter(self, chat_id):
+      last_used_town = self.used_towns[chat_id][-1]
+      return last_used_town[-1]
+
     def Clear(self, chat_id):
       self.used_towns = dict() 
 
@@ -89,7 +115,7 @@ class UsedTownsData:
 class DataBaseTowns:
 
     def Search(self, town_name):
-      with open('data/world-cities.csv', 'r') as f:
+      with open('./data/world-cities.csv', 'r') as f:
         csvreader = csv.reader(f, delimiter=',')
         for row in csvreader:
           if row[0] == town_name:
--- bot.py
+++ bot.py
@@ -5,47 +5,77 @@ from Data_Logic import UsedTownsData
 from Data_Logic import UsersData
 from Data_Logic import DataBaseTowns
 
-GameState = States.game_off
-
-
-
-UsedTownsData #Operates with towns used during game
-DataBaseTowns #Operates with towns database
-UsersData #Operates with registered users
+UsedTowns = UsedTownsData() #Operates with towns used during game
+DataTowns = DataBaseTowns() #Operates with towns database
+UserData = UsersData() #Operates with registered users
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=["start"])
-def greeting(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, '''Прывітанне! Гэта бот для гульні
-     ў гарады, для атрымання даведкі напішыце /help''')
+def greeting(message): 
+    bot.send_message(message.chat.id, '''Прывітанне! Гэта бот
+для гульні ў гарады, для атрымання даведкі напішыце /help''')
 
 @bot.message_handler(commands=["help"])
-def greeting(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, '''Правілы гульні стандартныя: \n
-    /reggame - пачаць новую гульню \n /endgame - скончыць гульню \n
-    /nextturn - зрабіць ход, рабіць хады па-за чаргой нельга \n
-    /usedtowns - спіс выкарыстаных гарадоў''')
+def greeting(message): 
+    bot.send_message(message.chat.id, '''Правілы гульні стандартныя:\n
+/reggame - пачаць новую гульню \n /endgame - скончыць гульню\n
+/nextturn - зрабіць ход, рабіць хады па-за чаргой нельга\n
+/usedtowns - спіс выкарыстаных гарадоў''')
 
 @bot.message_handler(commands=["reggame"])
 def start_game(message): 
-    if GameState.name == States.game_on:
+    id = message.chat.id
+    if UserData.CheckState(id) == True:
         bot.send_message(message.chat.id, "Спачатку завяршыце старую гульню")
     else:
-        GameState = States.game_on
+        UserData.AddChatGame(id)
         bot.send_message(message.chat.id, "Увядзіце колькасць удзельнікаў")
         bot.register_next_step_handler(message, get_players_numb)
 
 def get_players_numb(message):
-    players_numb = int(message.text)
-    for i in range(0, players_numb):
-        bot.register_next_step_handler(message, get_players_numb)
+    id = message.chat.id
+    UserData.AddPlayersNumb(id, int(message.text))
+    bot.send_message(message.chat.id, "Чтобы добавиться в игру вызови метод  /addme")
 
-def 
+@bot.message_handler(commands=["addme"])
+def add_player(message):
+    id = message.chat.id
+    if UserData.CheckState(id) == False:
+        bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
+        return   
+    if UserData.Check(id) ==  True:
+        UserData.AddUser(id, message.from_user.id) 
+        if UserData.Check(id) == False:
+            UsedTowns.Start(id)
+            UsedTowns.Add(id, 'Minsk')
+            bot.send_message(message.chat.id, "Игра началась! Для хода используйте /nextturn Первый город Minsk")
+    else:
+        bot.send_message(message.chat.id, "Места закончились")
 
+@bot.message_handler(commands=["nextturn"])
+def make_turn(message):
+    id = message.chat.id
+    if UserData.CheckState(id) == False:
+        bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
+        return
+    bot.send_message(message.chat.id, "Введи город на " + UsedTowns.GetLetter(id))
+    bot.register_next_step_handler(message, accept_town)
 
-#bot.send_message(message.chat.id, "Гульня пачалася! Назвы гарадоў неабходна пісаць на англійскай")
-
+def accept_town(message):
+    id = message.chat.id
+    town_name = message.text
+    if UserData.CheckTurn(id, message.from_user.id) == False:
+        bot.send_message(message.chat.id, "Не твой ход браток")
+    elif UsedTowns.CheckCorrectLetter(id, town_name) == False:
+        bot.send_message(message.chat.id, "Неверная буква")
+    elif UsedTowns.Find(id, town_name) == True:
+        bot.send_message(message.chat.id, "Город уже использован")
+    elif DataTowns.Search(town_name) == False:
+        bot.send_message(message.chat.id, "Такого города не существует")
+    else:
+        UserData.NextStep(id)
+        UsedTowns.Add(id, town_name)
 
 
 
@@ -54,9 +84,7 @@ def
 def start_game(message): 
     bot.send_message(message.chat.id, greeting)
 
-@bot.message_handler(commands=["nextturn"])
-def start_game(message): 
-    bot.send_message(message.chat.id, greeting)
+
 
 @bot.message_handler(commands=["usedtowns"])
 def start_game(message): 
