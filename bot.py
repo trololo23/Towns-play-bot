9520dbf9e34c947a61181131e848369cb9d6fd32
Trololo23 <egorbeketov37@gmail.com>
2022-12-22 03:23:40 +0300

    Added async

--- bot.py
+++ bot.py
@@ -1,6 +1,14 @@
-import telebot
 import config
-from Data_Logic import States
+
+import aiogram.utils.markdown as md
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+
 from Data_Logic import UsedTownsData
 from Data_Logic import UsersData
 from Data_Logic import DataBaseTowns
@@ -9,90 +17,90 @@ UsedTowns = UsedTownsData() #Operates with towns used during game
 DataTowns = DataBaseTowns() #Operates with towns database
 UserData = UsersData() #Operates with registered users
 
-bot = telebot.TeleBot(config.token)
+bot = Bot(config.token)
 
-@bot.message_handler(commands=["start"])
-def greeting(message): 
-    bot.send_message(message.chat.id, '''Прывітанне! Гэта бот
-для гульні ў гарады, для атрымання даведкі напішыце /help''')
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-@bot.message_handler(commands=["help"])
-def greeting(message): 
-    bot.send_message(message.chat.id, '''Правілы гульні стандартныя:\n
-/reggame - пачаць новую гульню \n /endgame - скончыць гульню\n
-/nextturn - зрабіць ход, рабіць хады па-за чаргой нельга\n
-/usedtowns - спіс выкарыстаных гарадоў''')
+@dp.message_handler(commands=["start"])
+async def greeting(message): 
+    await bot.send_message(message.chat.id, "Прывітанне! Гэта бот для гульні ў гарады, для атрымання даведкі напішыце /help")
 
-@bot.message_handler(commands=["reggame"])
-def start_game(message): 
+@dp.message_handler(commands=["help"])
+async def help(message): 
+    await bot.send_message(message.chat.id, "Правілы гульні стандартныя:\n/reggame - пачаць новую гульню\n/endgame - скончыць гульню\n/nextturn - зрабіць ход, рабіць хады па-за чаргой нельга\n/usedtowns - спіс выкарыстаных гарадоў")
+
+class RegState(StatesGroup):
+	active = State()
+
+@dp.message_handler(commands=["reggame"])
+async def start_game(message): 
+    await RegState.active.set()
     id = message.chat.id
     if UserData.CheckState(id) == True:
-        bot.send_message(message.chat.id, "Спачатку завяршыце старую гульню")
+        await bot.send_message(message.chat.id, "Спачатку завяршыце старую гульню")
     else:
         UserData.AddChatGame(id)
-        bot.send_message(message.chat.id, "Увядзіце колькасць удзельнікаў")
-        bot.register_next_step_handler(message, get_players_numb)
+        await bot.send_message(message.chat.id, "Увядзіце колькасць удзельнікаў")
 
-def get_players_numb(message):
+@dp.message_handler(state=RegState.active, content_types=types.ContentType.all())
+async def get_players_numb(message: types.Message, state: FSMContext):
     id = message.chat.id
     UserData.AddPlayersNumb(id, int(message.text))
-    bot.send_message(message.chat.id, "Чтобы добавиться в игру вызови метод  /addme")
+    await bot.send_message(message.chat.id, "Чтобы добавиться в игру вызови метод  /addme")
+    await state.finish()
 
-@bot.message_handler(commands=["addme"])
-def add_player(message):
+@dp.message_handler(commands=["addme"])
+async def add_player(message):
     id = message.chat.id
     if UserData.CheckState(id) == False:
-        bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
+        await bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
         return   
     if UserData.Check(id) ==  True:
         UserData.AddUser(id, message.from_user.id) 
         if UserData.Check(id) == False:
             UsedTowns.Start(id)
             UsedTowns.Add(id, 'Minsk')
-            bot.send_message(message.chat.id, "Игра началась! Для хода используйте /nextturn Первый город Minsk")
+            await bot.send_message(message.chat.id, "Игра началась! Для хода используйте /nextturn Первый город Minsk")
     else:
-        bot.send_message(message.chat.id, "Места закончились")
+        await bot.send_message(message.chat.id, "Места закончились")
 
-@bot.message_handler(commands=["nextturn"])
-def make_turn(message):
+class NextTurn(StatesGroup):
+	active = State()
+
+@dp.message_handler(commands=["nextturn"])
+async def make_turn(message):
+    await NextTurn.active.set()
     id = message.chat.id
     if UserData.CheckState(id) == False:
-        bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
+        await bot.send_message(message.chat.id, "Сначала зарегистрируйте игру")
         return
-    bot.send_message(message.chat.id, "Введи город на " + UsedTowns.GetLetter(id))
-    bot.register_next_step_handler(message, accept_town)
+    await bot.send_message(message.chat.id, "Введи город на " + UsedTowns.GetLetter(id))
 
-def accept_town(message):
+@dp.message_handler(state=NextTurn.active, content_types=types.ContentType.all())
+async def accept_town(message: types.Message, state: FSMContext):
     id = message.chat.id
     town_name = message.text
     if UserData.CheckTurn(id, message.from_user.id) == False:
-        bot.send_message(message.chat.id, "Не твой ход браток")
+        await bot.send_message(message.chat.id, "Не твой ход браток")
     elif UsedTowns.CheckCorrectLetter(id, town_name) == False:
-        bot.send_message(message.chat.id, "Неверная буква")
+        await bot.send_message(message.chat.id, "Неверная буква")
     elif UsedTowns.Find(id, town_name) == True:
-        bot.send_message(message.chat.id, "Город уже использован")
+        await bot.send_message(message.chat.id, "Город уже использован")
     elif DataTowns.Search(town_name) == False:
-        bot.send_message(message.chat.id, "Такого города не существует")
+        await bot.send_message(message.chat.id, "Такого города не существует")
     else:
         UserData.NextStep(id)
         UsedTowns.Add(id, town_name)
+        await bot.send_message(message.chat.id, "Харош")
 
+    await state.finish()
 
+@dp.message_handler(commands=["endgame"])
+async def end_game(message):
+    await bot.send_message(message.chat.id, "Игра завершена!")
+    UsedTowns.Clear()
+    UserData.Clear()
 
-
-@bot.message_handler(commands=["endgame"])
-def start_game(message): 
-    bot.send_message(message.chat.id, greeting)
-
-
-
-@bot.message_handler(commands=["usedtowns"])
-def start_game(message): 
-    bot.send_message(message.chat.id, greeting)
-
-
-
-
-bot.polling(none_stop=True, interval=0)
-
-
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
